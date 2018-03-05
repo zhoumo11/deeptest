c89598c2a086e5ec4fde2bd7e01fa586c868db03
zhoumo11 <1187649868@qq.com>
2018-03-05 23:09:47 +0800

    第一天

---
+++ 第二期/北京-爱笑的蝈蝈/day_01/task_01.py
@@ -0,0 +1,16 @@
+#数值转换
+# _*_ coding:utf-8 _*_
+__author__ = 'zhoull'
+
+if __name__ == '__main__':
+    x = 16.80
+    y = 10
+
+    #将x转换为整数
+    print(int(x))
+
+    #将y转换为浮点数
+    print(float(y))
+
+    #将x,y转换为复数，x为实数部分，y为虚数部分
+    print(complex(x,y))
---
+++ 第二期/北京-爱笑的蝈蝈/day_01/task_02.py
@@ -0,0 +1,34 @@
+#数值函数表示
+
+import math
+import random
+# _*_ coding:ytf-8 _*_
+if __name__ == '__main__':
+    x = -12.39
+    y = 3.44
+    z = 100
+
+    #获取某数的绝对值
+    print(abs(x))
+
+    #返回最大值
+    print(max(x,y,z))
+
+    #返回最小值
+    print(min(x,y,z))
+
+    #计算y的平方
+    print(pow(y,2))
+
+    #返回平方根
+    print(math.sqrt(z))
+
+    #随机数
+    a = [1,2,3,4,5,6,7,8,9,0]
+    print(random.choice(a))
+
+    #从指定范围（2-100）按5递增的数据中，随机选择一个
+    print(random.randrange(2,100,5))
+
+    #生成一个随机数，在（0，1）之间
+    print(random.random())
